d319d7674a238718ebe87cd7e7e4584fffb1c324
Gordon <gordon.nadav@gmail.com>
2017-12-06 14:33:45 +0200

    init emailsPage

---
+++ pages/EmailsPage.js
@@ -0,0 +1,8 @@
+'use strict' // Start of use strict
+
+export default {
+    template: `
+    <h1>Emails App</h1> 
+    
+    `
+}
--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 'use strict'
 import HomePage from './pages/HomePage.js'
+import EmailsPage from './pages/EmailsPage.js'
 
 const routes = [
     {
         path: '/',
         component: HomePage
-    }  
+    }, 
+    {
+        path: '/emails',
+        component: EmailsPage
+    }
    
 
 ];
