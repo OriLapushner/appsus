4eed9aded606a9ef6fcf1cc141323552ec6f665a
OriLapushner <ori.lapushner@gmail.com>
2017-12-06 14:36:22 +0200

    update pages

--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 'use strict'
 import HomePage from './pages/HomePage.js'
+import PlacesPage from './pages/PlacesPage.js'
 
 const routes = [
     {
         path: '/',
         component: HomePage
-    }  
+    },
+    {
+        path: '/places',
+        component: PlacesPage
+    }
    
 
 ];
