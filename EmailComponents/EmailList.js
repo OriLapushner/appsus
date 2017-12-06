743d7f89c56b21d2ed28909905f4a7d3ce15ac32
OriLapushner <ori.lapushner@gmail.com>
2017-12-06 18:36:34 +0200

    mail update

--- EmailComponents/EmailList.js
+++ EmailComponents/EmailList.js
@@ -6,15 +6,22 @@ export default {
 
     template: `
                 <div>
-                <email-prev v-for="mail in mails" :mail="mail"></email-prev>
+                <input type="text">
+                <button>arrange by date</button>
+                <button>newest first</button>
+                <email-prev v-for="mail in mails" :mail="mail" class="email-prev"></email-prev>
                 </div>
     `,
-    props: ['email'],
     data() {
         return {
-            mails: EmailService.getMails()
+            mails: null
 
         }
+    },
+    created(){
+        EmailService.getMails()
+        .then(mails =>
+            this.mails = mails)
     },
     components: {
         EmailPrev
--- services/EmailService.js
+++ services/EmailService.js
@@ -2,42 +2,42 @@
 var mailList = [
     {
     id:0,
-    sentBy: 'email adress string',
-    title: 'some string',
+    sentBy: 'sentBy',
+    title: 'title',
     isRead: false,
     sentAt: Date.now(),
     content:'body of email text',
 },
 {
     id:1,
-    sentBy: 'email adress string1',
-    title: 'some string1',
+    sentBy: 'sentBy1',
+    title: 'title1',
     isRead: false,
     sentAt: Date.now(),
-    content:'body of email text1',
+    content:'content1',
 },
 {
     id:2,
-    sentBy: 'email adress string2',
-    title: 'some string2',
+    sentBy: 'sentBy2',
+    title: 'title2',
     isRead: false,
     sentAt: Date.now(),
-    content:'body of email text2',
+    content:'content2',
 },
 {
     id:3,
-    sentBy: 'email adress string3',
-    title: 'some string3',
+    sentBy: 'sentBy3',
+    title: 'title3',
     isRead: false,
     sentAt: Date.now(),
-    content:'body of email text3',
+    content:'content3',
 },
 ]
 function getMails(){
     return new Promise((resolve,reject) =>{
     setTimeout(()=> {resolve(mailList)
         
-    }, timeout);
+    }, 1000);
 })
 }
 
