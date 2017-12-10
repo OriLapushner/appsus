0db3674229a3ff220427206e190483fb6d313af3
OriLapushner <ori.lapushner@gmail.com>
2017-12-10 15:55:17 +0200

    ss

--- EmailComponents/EmailMenu.js
+++ EmailComponents/EmailMenu.js
@@ -3,7 +3,6 @@ import EventBusService from '../services/EventBusService.js'
 export default {
     template: `
     <div>
-<<<<<<< HEAD
         <button @click="toggleCompose" class="compose-btn">composeMail</button><br>
         <button @click="markMails" class="mark-read-btn">mark as read</button><br>
         <button @click="deleteMails" class="email-delete-btn">
@@ -13,14 +12,6 @@ export default {
         <input type="text" placeholder="search email" @keyup="updateEmailList" v-model="searchText">
         <i class="fa fa-search" aria-hidden="true"></i>
         
-=======
-        <button @click="toggleCompose">composeMail</button>
-        <i class="fa fa-trash-o delete-btn" aria-hidden="true" @click="deleteMails"></i>        
-        <button @click="markMails">mark as read</button>         
-        <input type="text" placeholder="search email" @change="updateEmailList" v-model="searchText">
-        <button @click="sortByDate">arrange by date</button>
-        <button>unread first</button>
->>>>>>> ca8b8d79ed708e1575620024e6df05811b239426
     </div>
 `,
 
